2aba58eed47d13e86e58bc1a743fada81e749fed
Garthigan <garthigankumarasamy11@gmail.com>
2024-02-09 15:23:17 +0530

    Done - 9 Feb

--- Attendance.py
+++ Attendance.py
@@ -52,17 +52,19 @@ while True:
     for encodeFac,faceLoc in zip(encodeCurFrame,faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnow,encodeFac)
         faceDis = face_recognition.face_distance(encodeListKnow,encodeFac)
-        print(faceDis)
+        #print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = class_names[matchIndex].upper()
-            print(name)
+            #print(name)
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255),2)
+            MarkAttendance(name)
+
 
     cv2.imshow('WebCam',img)
     cv2.waitKey(1)
